b3714a3974fba32c33f7981bbde877ae398e47dd
mnsfergie95 <53720256+mnsfergie95@users.noreply.github.com>
2023-12-06 17:21:18 -0800

    Add files via upload

--- searcher/sql/storagetooRtns.sql
+++ searcher/sql/storagetooRtns.sql
@@ -20,21 +20,10 @@
 /*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+USE storagetoo;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `DropUserIfExists`(MyUserName VARCHAR(100))
-BEGIN
-  DECLARE foo BIGINT DEFAULT 0 ;
-  SELECT COUNT(*)
-  INTO foo
-    FROM mysql.user
-      WHERE User = MyUserName ;
-   IF foo > 0 THEN
-         SET @A = (SELECT Result FROM (SELECT GROUP_CONCAT("DROP USER"," ",MyUserName,"@'localhost'") AS Result) AS Q LIMIT 1);
-         PREPARE STMT FROM @A;
-         EXECUTE STMT;
-         FLUSH PRIVILEGES;
-   END IF;
-END ;;
+BEGIN  DECLARE foo BIGINT DEFAULT 0 ;  SELECT COUNT(*)  INTO foo    FROM mysql.user      WHERE User = MyUserName ;   IF foo > 0 THEN         SET @A = (SELECT Result FROM (SELECT GROUP_CONCAT("DROP USER"," ",MyUserName,"@'localhost'") AS Result) AS Q LIMIT 1);         PREPARE STMT FROM @A;         EXECUTE STMT;         FLUSH PRIVILEGES;   END IF;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -50,11 +39,7 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procAddLessee`(IN `Name` VARCHAR(30), IN `Street1` VARCHAR(30), IN `Street2` VARCHAR(30), IN `City` VARCHAR(30), IN `State` VARCHAR(30), IN `Zip` VARCHAR(12), IN `Phone` VARCHAR(15), IN `UnitLbl` VARCHAR(5))
-BEGIN
-	DECLARE uid int;
-	SELECT unit.unitid INTO uid FROM unit WHERE unit.label=UnitLbl;
-	INSERT INTO lessee VALUES (uid,Name,Street1,Street2,City,State,Zip,Phone,1);
-END ;;
+BEGIN	DECLARE uid int;	SELECT unit.unitid INTO uid FROM unit WHERE unit.label=UnitLbl;	INSERT INTO lessee VALUES (uid,Name,Street1,Street2,City,State,Zip,Phone,1);END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -71,9 +56,7 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procDeactivateLessee`(IN `lesseeId` INT)
     NO SQL
-BEGIN
-UPDATE lessee SET active = 0 WHERE lessee.lessee_id = lesseeId;
-END ;;
+BEGIN UPDATE lessee SET active = 0 WHERE lessee.lessee_id = lesseeId;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -90,9 +73,7 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procFindUid`(IN `lbl` VARCHAR(10))
     NO SQL
-BEGIN
-Select unitid from unit where label = lbl;
-END ;;
+BEGIN Select unitid from unit where label = lbl;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -108,9 +89,7 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procGenerateLastPayment`(IN `unitTitle` VARCHAR(10))
-BEGIN
-	SELECT * FROM payment LEFT JOIN unit ON payment.unit_id = unit.unitid WHERE payment.pmt_date = (SELECT MAX(pmt_date) FROM payment WHERE payment.unit_id = (SELECT unit.unitid FROM unit WHERE unit.label = unitTitle));
-END ;;
+BEGIN	SELECT * FROM payment LEFT JOIN unit ON payment.unit_id = unit.unitid WHERE payment.pmt_date = (SELECT MAX(pmt_date) FROM payment WHERE payment.unit_id = (SELECT unit.unitid FROM unit WHERE unit.label = unitTitle));END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -126,10 +105,7 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procGetAllLeasedUnitIDs`()
-BEGIN
-	SELECT unitid FROM unit LEFT JOIN lessee ON lessee.unitid = unit.unitid
-	WHERE lessee.active = 1;
-END ;;
+BEGIN	SELECT unitid FROM unit LEFT JOIN lessee ON lessee.unitid = unit.unitid	WHERE lessee.active = 1;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -146,11 +122,7 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procGetAllPaymentsOnUnit`(IN `unitTitle` VARCHAR(10))
     NO SQL
-BEGIN
-SELECT * FROM payment LEFT JOIN unit ON
-payment.unit_id = unit.unitid 
-WHERE unit.label = unitTitle;
-END ;;
+BEGIN SELECT * FROM payment LEFT JOIN unit ON payment.unit_id = unit.unitid WHERE unit.label = unitTitle;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -166,10 +138,7 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procListAllAvailUnits`()
-BEGIN
-	SELECT label FROM unit LEFT JOIN lessee ON lessee.unitid = unit.unitid 
-	WHERE lessee.lesseeid IS NULL OR lessee.active = 0;
-END ;;
+BEGIN	SELECT label FROM unit LEFT JOIN lessee ON lessee.unitid = unit.unitid 	WHERE lessee.lesseeid IS NULL OR lessee.active = 0;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -185,10 +154,7 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procListAllLeasedUnits`()
-BEGIN
-	SELECT label FROM unit LEFT JOIN lessee ON lessee.unit_id = unit.unitid
-	WHERE lessee.active = 1;
-END ;;
+BEGIN	SELECT label FROM unit LEFT JOIN lessee ON lessee.unit_id = unit.unitid	WHERE lessee.active = 1;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -205,9 +171,7 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procLogin`(IN `uname` VARCHAR(30))
     NO SQL
-BEGIN 
-SELECT id, username, pwd FROM users WHERE username = uname;
-END ;;
+BEGIN SELECT id, username, pwd FROM users WHERE username = uname;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -224,9 +188,7 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procMonthly`(IN `lbl` VARCHAR(10))
     NO SQL
-BEGIN
-select monthly_price from pricing join unit where pricing.sizeid = unit.sizeid and unit.label = lbl;
-END ;;
+BEGIN select monthly_price from pricing join unit where pricing.sizeid = unit.sizeid and unit.label = lbl;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -242,12 +204,7 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procNumAvailUnits`(Size INTEGER)
-BEGIN
-	SELECT Count(unit.label)
-	FROM unit
-	WHERE sizeid = Size AND unit.unitid NOT IN 
-	(SELECT unit.unitid FROM unit, lessee WHERE unit.unitid = lessee.unitid AND lessee.active = 1);
-END ;;
+BEGIN	SELECT Count(unit.label)	FROM unit	WHERE sizeid = Size AND unit.unitid NOT IN 	(SELECT unit.unitid FROM unit, lessee WHERE unit.unitid = lessee.unitid AND lessee.active = 1);END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -264,9 +221,7 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procPutPmt`(IN `amt` DECIMAL(5,2), IN `pmtdate` DATE, IN `pmtapplydate` DATE, IN `uid` INT(10))
     NO SQL
-BEGIN
-INSERT INTO payment (pmt_amount, pmt_date, pmt_applydate, unitid) VALUES (amt, pmtdate, pmtapplydate, uid);
-END ;;
+BEGIN INSERT INTO payment (pmt_amount, pmt_date, pmt_applydate, unitid) VALUES (amt, pmtdate, pmtapplydate, uid);END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -283,9 +238,7 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procResetPassword`(IN `pword` BINARY(72), IN `userid` INT)
     NO SQL
-BEGIN
-UPDATE users SET pwd = pword WHERE id = userid;
-END ;;
+BEGIN UPDATE users SET pwd = pword WHERE id = userid;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -300,19 +253,8 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `procSearch`(
-  Name VARCHAR(30),
-  Unit VARCHAR(5),
-  Phone VARCHAR(15) )
-BEGIN
-	SELECT lessee.*, unit.sizeid		
-	FROM lessee JOIN unit ON lessee.unitid = unit.unitid
-	WHERE lessee.unitid = Unit
-	OR
-	lessee.lesseename = Name
-	OR
-	lessee.phone = Phone;
-END ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `procSearch`(  Name VARCHAR(30),  Unit VARCHAR(5),  Phone VARCHAR(15) )
+BEGIN	SELECT lessee.*, unit.sizeid			FROM lessee JOIN unit ON lessee.unitid = unit.unitid	WHERE lessee.unitid = Unit	OR	lessee.lesseename = Name	OR	lessee.phone = Phone;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -328,10 +270,7 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procSelectAllAvailableUnits`()
-BEGIN
-    SELECT label,descr FROM unit LEFT JOIN lessee ON lessee.unitid = unit.unitid LEFT JOIN sizes ON sizes.sizeid = unit.sizeid 
-	WHERE lessee.lesseeid IS NULL OR lessee.active = 0;
-END ;;
+BEGIN    SELECT label,descr FROM unit LEFT JOIN lessee ON lessee.unitid = unit.unitid LEFT JOIN sizes ON sizes.sizeid = unit.sizeid 	WHERE lessee.lesseeid IS NULL OR lessee.active = 0;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -347,10 +286,7 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procSelectAllLeasedUnits`()
-BEGIN
-    SELECT label,descr FROM unit LEFT JOIN lessee ON lessee.unitid = unit.unitid LEFT JOIN sizes ON sizes.sizeid = unit.sizeid 
-	WHERE lessee.active = 1 ORDER BY label;
-END ;;
+BEGIN    SELECT label,descr FROM unit LEFT JOIN lessee ON lessee.unitid = unit.unitid LEFT JOIN sizes ON sizes.sizeid = unit.sizeid 	WHERE lessee.active = 1 ORDER BY label;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -366,10 +302,7 @@ DELIMITER ;
 /*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procSelectLesseeNames`()
-BEGIN
-    SELECT lesseename FROM lessee
-	WHERE lessee.active = 1;
-END ;;
+BEGIN    SELECT lesseename FROM lessee	WHERE lessee.active = 1;END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -386,9 +319,7 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `procTest`(IN `pword` VARCHAR(30))
     NO SQL
-BEGIN
-UPDATE users SET pwd = pword WHERE username = 'demo';
-END ;;
+BEGIN UPDATE users SET pwd = pword WHERE username = 'demo';END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -401,3 +332,11 @@ DELIMITER ;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2023-11-26 19:11:15
+
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `procCommitPmt`(IN `unitTitle` VARCHAR(10), IN `amt` DECIMAL(5,2), IN `pmtDate` DATE, IN `nextDueDate` DATE)
+    NO SQL
+BEGIN SELECT unitid INTO @uid FROM unit WHERE label=unitTitle;INSERT INTO payment VALUES (0, amt, pmtDate, nextDueDate, @uid);END ;;
+DELIMITER ;
+
+CALL procCommitPmt('c1',78.23,("23-12-06"),("24-01-01"));
