9ae5090ef6c2ecc6a0555b2fc3fe58149d3937bf
mnsfergie95 <53720256+mnsfergie95@users.noreply.github.com>
2023-12-19 19:06:03 -0800

    Add files via upload

---
+++ searcher/src/main/java/searcher/Possible.java
@@ -0,0 +1,11 @@
+package searcher;
+
+public class Possible {
+
+    //int rowsAffected = stmt.executeUpdate();
+    //System.out.println("rowsAffected is " + rowsAffected);
+
+    //Convert string lesseeId to integer
+    //Integer lesseeID = Integer.valueOf(lesseeId);
+    
+}
